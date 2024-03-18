151cc1a299caa806ee790975acd3e4721d7f2b3d
Zaruto <iamismailzaid@gmail.com>
2024-03-18 13:31:22 +0500

    header action

--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Resources;
 
+use App\Actions\Attendance\SyncAttendance;
+use App\Actions\Attendance\SyncAttendanceToServer;
 use App\Filament\Resources\AttendanceResource\Pages;
 use App\Models\Attendance;
+use Filament\Actions\Action;
 use Filament\Forms\Components\DatePicker;
 use Filament\Resources\Resource;
+use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\BulkAction;
 use Filament\Tables\Actions\BulkActionGroup;
 use Filament\Tables\Actions\DeleteBulkAction;
@@ -37,6 +41,19 @@ class AttendanceResource extends Resource
         return $table
             ->paginated([15, 50, 100])
             ->defaultSort('action_at', 'desc')
+            ->headerActions([
+                ActionGroup::make([
+                    Action::make('Server')
+                        ->label('Upload')
+                        ->icon('heroicon-s-arrow-up-tray')
+                        ->action(fn() => SyncAttendanceToServer::run()),
+
+                    Action::make('Devices')
+                        ->label('Download')
+                        ->icon('heroicon-s-arrow-down-tray')
+                        ->action(fn() => SyncAttendance::run()),
+                ]),
+            ])
             ->columns([
                 TextColumn::make('user.name')->label('User')->searchable()->sortable(),
 
@@ -46,7 +63,7 @@ class AttendanceResource extends Resource
 
                 TextColumn::make('action')
                     ->badge()
-                    ->color(fn (string $state): string => match ($state) {
+                    ->color(fn(string $state): string => match ($state) {
                         'default' => 'warning',
                         'Check-in' => 'success',
                         'Check-out' => 'danger',
@@ -55,12 +72,12 @@ class AttendanceResource extends Resource
                 IconColumn::make('sync_at')
                     ->label('Synced')
                     ->icons([
-                        'heroicon-o-x-circle' => fn ($state): bool => $state == 0,
-                        'heroicon-s-check-circle' => fn ($state): bool => $state != 0,
+                        'heroicon-o-x-circle' => fn($state): bool => $state == 0,
+                        'heroicon-s-check-circle' => fn($state): bool => $state != 0,
                     ])
                     ->colors([
-                        'danger' => fn ($state): bool => $state == 0,
-                        'success' => fn ($state): bool => $state != 0,
+                        'danger' => fn($state): bool => $state == 0,
+                        'success' => fn($state): bool => $state != 0,
                     ])
                     ->sortable(),
             ])
@@ -73,7 +90,7 @@ class AttendanceResource extends Resource
                         ->icon('heroicon-o-arrow-path-rounded-square')
                         ->requiresConfirmation()
                         ->deselectRecordsAfterCompletion()
-                        ->action(fn (Collection $records) => $records->each->update(['sync_at' => 0])),
+                        ->action(fn(Collection $records) => $records->each->update(['sync_at' => 0])),
 
                     DeleteBulkAction::make(),
                 ]),
@@ -96,10 +113,10 @@ class AttendanceResource extends Resource
                     ->query(function (Builder $query, array $data): Builder {
                         return $query
                             ->when($data['date_from'],
-                                fn (Builder $query, $date): Builder => $query->whereDate('action_at', '>=', $date),
+                                fn(Builder $query, $date): Builder => $query->whereDate('action_at', '>=', $date),
                             )
                             ->when($data['date_to'],
-                                fn (Builder $query, $date): Builder => $query->whereDate('action_at', '<=', $date),
+                                fn(Builder $query, $date): Builder => $query->whereDate('action_at', '<=', $date),
                             );
                     }),
 
--- app/Filament/Resources/AttendanceResource/Pages/ListAttendance.php
+++ app/Filament/Resources/AttendanceResource/Pages/ListAttendance.php
@@ -13,22 +13,4 @@ class ListAttendance extends ListRecords
 {
     protected static string $resource = AttendanceResource::class;
 
-    protected function getActions(): array
-    {
-        return [
-            ActionGroup::make([
-
-                Action::make('Server')
-                    ->label('Upload')
-                    ->icon('heroicon-s-arrow-up-tray')
-                    ->action(fn () => SyncAttendanceToServer::run()),
-
-                Action::make('Devices')
-                    ->label('Download')
-                    ->icon('heroicon-s-arrow-down-tray')
-                    ->action(fn () => SyncAttendance::run()),
-            ]),
-
-        ];
-    }
 }
