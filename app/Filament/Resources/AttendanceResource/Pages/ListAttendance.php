ef076c5022069b5abee68e0dab481e5acb4dc21a
Zaruto <iamismailzaid@gmail.com>
2024-03-18 13:50:54 +0500

    attendance action

--- app/Filament/Resources/AttendanceResource/Pages/ListAttendance.php
+++ app/Filament/Resources/AttendanceResource/Pages/ListAttendance.php
@@ -16,18 +16,18 @@ class ListAttendance extends ListRecords
     protected function getActions(): array
     {
         return [
-            ActionGroup::make([
+//            ActionGroup::make([
 
-                Action::make('Server')
-                    ->label('Upload')
-                    ->icon('heroicon-s-arrow-up-tray')
-                    ->action(fn() => SyncAttendanceToServer::run()),
+            Action::make('Server')
+                ->label('Upload')
+                ->icon('heroicon-s-arrow-up-tray')
+                ->action(fn() => SyncAttendanceToServer::run()),
 
-                Action::make('Devices')
-                    ->label('Download')
-                    ->icon('heroicon-s-arrow-down-tray')
-                    ->action(fn() => SyncAttendance::run()),
-            ]),
+            Action::make('Devices')
+                ->label('Download')
+                ->icon('heroicon-s-arrow-down-tray')
+                ->action(fn() => SyncAttendance::run()),
+//            ]),
 
         ];
     }
