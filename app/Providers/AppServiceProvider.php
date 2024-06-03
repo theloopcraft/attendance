7532bf5ec535d16bfe44ccfa2c5528a49d05f3ad
Zaruto <iamismailzaid@gmail.com>
2024-06-03 11:14:41 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->isProduction()) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
