6395de944af8d89511753c6a51e2b1b21de7f88b
Zaruto <iamismailzaid@gmail.com>
2025-02-19 16:54:07 +0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
+
+    ini_set('max_execution_time', 600);
+    ini_set('memory_limit', '-1');
+
     $allData = [];
     $attendances = Attendance::query()->latest()->first();
     $startAt = Carbon::now()->startOfDay()->toDateTimeString();
