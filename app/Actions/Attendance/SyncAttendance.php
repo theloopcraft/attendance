05339548a2e514bf0a31be7566aac3495b43c14d
Zaruto <iamismailzaid@gmail.com>
2025-03-13 13:08:45 +0500

    wip

--- app/Actions/Attendance/SyncAttendance.php
+++ app/Actions/Attendance/SyncAttendance.php
@@ -32,7 +32,7 @@ class SyncAttendance extends Action
     private function syncAttendanceData(): void
     {
         $lastAttendance = Attendance::query()->latest()->first();
-        $startAt = $lastAttendance ? Carbon::parse($lastAttendance->action_at)->startOfDay() : Carbon::now()->startOfDay();
+        $startAt = $lastAttendance ? Carbon::parse($lastAttendance->action_at)->startOfMonth()->startOfDay() : Carbon::now()->startOfMonth()->startOfDay();
         $endAt = $startAt->copy()->addDay()->endOfDay();
 
 
