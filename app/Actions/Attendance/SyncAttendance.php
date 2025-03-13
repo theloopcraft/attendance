b2e8b2b28e5562e844a679e2165b7023bd1421e9
Zaruto <iamismailzaid@gmail.com>
2025-03-13 12:25:52 +0500

    wip

--- app/Actions/Attendance/SyncAttendance.php
+++ app/Actions/Attendance/SyncAttendance.php
@@ -66,7 +66,7 @@ class SyncAttendance extends Action
 
             if (empty($allData)) {
                 Log::warning("No data found for {$startAt} to {$endAt}. Incrementing date...");
-                $startAt = Carbon::parse($startAt)->addDay()->startOfDay()->toDateTimeString();
+                $startAt = Carbon::parse($startAt)->startOfDay()->toDateTimeString();
                 $endAt = Carbon::parse($endAt)->addDay()->endOfDay()->toDateTimeString();
                 $retryCount++;
             }
