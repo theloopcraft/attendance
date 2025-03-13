674f1af9e889d87347717b860a166de365413726
Zaruto <iamismailzaid@gmail.com>
2025-03-13 13:20:26 +0500

    wip

--- app/Actions/Attendance/SyncAttendance.php
+++ app/Actions/Attendance/SyncAttendance.php
@@ -42,6 +42,8 @@ class SyncAttendance extends Action
 
             $allData = $this->fetchAttendanceData($startAt, $endAt);
 
+            dd($allData);
+            
             if (!empty($allData)) {
                 $this->processAttendanceData($allData);
                 Log::info("Attendance sync completed for period: $startAt to $endAt.");
