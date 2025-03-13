ed318f79e135df955e7fa9dac62eb3d0ee37ae83
Zaruto <iamismailzaid@gmail.com>
2025-03-13 13:04:02 +0500

    wip

--- app/Actions/Attendance/SyncAttendance.php
+++ app/Actions/Attendance/SyncAttendance.php
@@ -35,7 +35,7 @@ class SyncAttendance extends Action
         $startAt = $lastAttendance ? Carbon::parse($lastAttendance->action_at)->startOfDay() : Carbon::now()->startOfDay();
         $endAt = $startAt->copy()->addDays(3)->endOfDay();
 
-        dd($startAt, $endAt);
+//        dd($startAt, $endAt);
 
         $retryCount = 0;
 
